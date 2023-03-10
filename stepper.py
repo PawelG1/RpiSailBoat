db76138ce0d7c8bc775220ad7fcbdc73b4092f16
PawelG1 <pawelgalka153@gmail.com>
2023-03-10 14:47:57 +0100

    (feat) add new module for winch usage

---
+++ stepper.py
@@ -0,0 +1,65 @@
+from math import degrees
+import pigpio
+import time
+pi = pigpio.pi()
+
+class stepper():
+	def __init__(self) -> None:
+		self.STEP_PIN = 19
+		self.DIR_PIN = 13
+		self.stepPTurn = 1324 #steps per 1 turn
+		#stepper motor steps per rotation =200 , gearbox ratio 324/49
+		self.delay = 0.0025
+
+	def pull(self, degrees): # input have to be in degrees
+		steps = int((degrees / 360) * self.stepPTurn)
+		
+		pi.write(self.DIR_PIN, pigpio.HIGH)
+
+		for i in range(steps):
+			time.sleep(self.delay)
+			pi.write(self.STEP_PIN, 1)
+			time.sleep(0.00001)
+			pi.write(self.STEP_PIN, 0)
+
+		time.sleep(0.00001)
+
+			
+	def loosen(self, degrees):
+		steps = int((degrees / 360) * self.stepPTurn)
+
+		pi.write(self.DIR_PIN, pigpio.LOW)
+
+		for i in range(steps):
+			time.sleep(self.delay)
+			pi.write(self.STEP_PIN, 1)
+			time.sleep(0.00001)
+			pi.write(self.STEP_PIN, 0)
+
+		time.sleep(0.00001)
+
+
+
+if __name__ == '__main__':
+
+	winch = stepper()
+	delay = 0.01
+	STEP_PIN = 19 
+	pi.write(13, pigpio.HIGH)
+
+
+	winch.pull(360)
+	time.sleep(2)
+	winch.loosen(360)
+
+	"""for i in range(1000):
+		time.sleep(delay)
+		pi.write(19, 1)
+		print("1")
+		time.sleep(0.001)
+		pi.write(19, 0)
+		print("0")
+	"""
+
+	  
+	
